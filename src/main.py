ebfb60808582c3aab72ea9bd964a2e4765282dad
mirkobrombin <send@mirko.pm>
2022-12-09 22:21:51 +0100

    0.2

--- src/main.py
+++ src/main.py
@@ -58,7 +58,7 @@ class VanillaControlCenterApplication(Adw.Application):
                                 application_name=_('Vanilla OS Control Center'),
                                 application_icon='org.vanillaos.ControlCenter',
                                 developer_name='Mirko Brombin',
-                                version='0.1.8',
+                                version='0.2',
                                 developers=['Mirko Brombin'],
                                 copyright='© 2022 Mirko Brombin')
         about.add_credit_section(
