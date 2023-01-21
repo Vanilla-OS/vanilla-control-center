9a5283c399700910a46cb83d9c21fe4648b068ce
mirkobrombin <send@mirko.pm>
2023-01-22 00:09:28 +0100

    1.3.1

--- vanilla_control_center/main.py
+++ vanilla_control_center/main.py
@@ -60,7 +60,7 @@ class VanillaControlCenterApplication(Adw.Application):
                                 application_name=_('Vanilla OS Control Center'),
                                 application_icon='org.vanillaos.ControlCenter',
                                 developer_name='Mirko Brombin',
-                                version='1.6.0',
+                                version='1.6.1',
                                 developers=['Mirko Brombin'],
                                 copyright='© 2023 Mirko Brombin',
                                 website='https://vanillaos.org/',
