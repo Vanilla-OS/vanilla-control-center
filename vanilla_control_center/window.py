ad01c0bb253e6240a6a00894d7077a3d4af662de
mirkobrombin <send@mirko.pm>
2023-01-29 15:53:02 +0100

    frontend: Toggle reboot and update buttons according to the update status

--- vanilla_control_center/window.py
+++ vanilla_control_center/window.py
@@ -51,6 +51,7 @@ class VanillaWindow(Adw.ApplicationWindow):
     status_updates = Gtk.Template.Child()
     btn_apply = Gtk.Template.Child()
     btn_update_now = Gtk.Template.Child()
+    btn_update_reboot = Gtk.Template.Child()
     toasts = Gtk.Template.Child()
     row_update_status = Gtk.Template.Child()
     combo_update_schedule = Gtk.Template.Child()
@@ -168,13 +169,20 @@ class VanillaWindow(Adw.ApplicationWindow):
         if auto := self.vso.auto:
             self.switch_update_auto.set_active(auto)
 
+        self.__reset_update_buttons()
+
         self.combo_update_schedule.connect("notify::selected", self.__on_update_schedule_changed)
         self.switch_update_smart.connect("state-set", self.__on_update_smart_changed)
         self.switch_update_auto.connect("state-set", self.__on_update_auto_changed)
         self.btn_update_now.connect("clicked", self.__on_update_now_clicked)
+    
+    def __reset_update_buttons(self):
+        self.btn_update_now.set_visible(self.vso.can_update)
+        self.btn_update_reboot.set_visible(not self.vso.can_update)
 
     def __on_update_now_clicked(self, widget):
         VanillaDialogUpdateCheck(self).show()
+        self.__reset_update_buttons()
         
     def __on_update_smart_changed(self, widget, state, *args):
         res = self.vso.set_smartupdate(state)
